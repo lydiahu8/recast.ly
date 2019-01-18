3980148c4ac1f05e945c27cd3924daa37196b95a
lydiahu8 <lydiahu8@gmail.com>
2019-01-17 21:43:14 -0800

    Implement reusable API helper function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,21 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET', 
+    data: {
+      q: options.query,
+      maxResults: options.max,
+      part: 'snippet',
+      key: options.key
+    },
+    videoEmbeddable: 'true',
+    success: function(data) {
+      callback(data.items);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
 };
 
 export default searchYouTube;
